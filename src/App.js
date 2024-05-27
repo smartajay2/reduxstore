8a17f818521f07adac895eeb7857726e60674fc8
smartajay2 <ajayjest2@gmail.com>
2024-05-27 18:31:31 +0400

    redux store completed

--- src/App.js
+++ src/App.js
@@ -1,24 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
-
+import './index.css';
+import { useSelector,useDispatch } from 'react-redux';
+import { increment, decrement } from './store'; 
 function App() {
+
+
+  const count = useSelector(function(state){
+    return state.counter.count
+  })
+
+  const dispatch = useDispatch()
+
+
+  function icfunction(){
+    dispatch(increment())
+  }
+
+  function defunction(){
+    dispatch( decrement())
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+     <>
+        <h1 className="text-3xl font-bold underline bg-yellow-200 ml-10 mr-10  p-4 text-center">Hello world Ajay!</h1>
+        <p className='text-center'>This is the page where i learn redux</p>
+    </>
+
+    <>
+        <h1 className='bg-green-300 ml-10 mr-10  p-2 text-center'>Hello texting div store from provider..</h1>
+        <h1 className='bg-green-300 ml-10 mr-10  p-2 pl-2 text-center'>Answer..{count}</h1>
+        <button className='block bg-slate-400 text-center pl-2 pr-2 rounded-md top-4 mt-3' onClick={icfunction}>Increment</button>
+        <button className='block bg-slate-400 text-center mt-3 pl-2 pr-2 rounded-md' onClick={defunction}>Decrement</button>
+    </>
+    </>
+   
   );
 }
 
